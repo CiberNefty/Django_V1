6962cf16c860e65d287c50c4c535dfcace423aef
Daniel Santiago Vera Arias <dsveraa.ing@gmail.com>
2026-03-09 11:06:33 -0500

    Feat: Modificamos los archivos tanto de platnilla como de vistas, en el cual trabajamos uso de variables en plantillas, accesos a objetos y propiedades desde una plantilla y el uso de diccionarios en contexto.

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -2,8 +2,20 @@ from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
 
+class Persona(object):
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
 # Creacion vista
 def saludo(request):
+
+    p1 = Persona("Santiago", "Gomez")
+
+    nombre = "Dani"
+    apellido = "Vera"
+    fecha_ahora = datetime.datetime.now()
+
     # Cargamos nuestra plantilla 
     doc_externo = open("C:/Users/danim/Proyectos/Django_pildoras/Proyecto1/Proyecto1/Plantillas/miplantilla1.html")
     #Creamos el objeto tipo Template
@@ -12,7 +24,8 @@ def saludo(request):
     doc_externo.close()
 
     # Crear contexto
-    contexto = Context()
+    contexto = Context({"nombre_persona": nombre, "apellido_persona": apellido, "apellido_2": "Arias", "fecha_ahora": fecha_ahora,
+                        "objeto_persona1_nom": p1.nombre, "objeto_persona1_ap": p1.apellido})
 
     # Renderizar plantilla (variable)
     documento = platilla.render(contexto)
