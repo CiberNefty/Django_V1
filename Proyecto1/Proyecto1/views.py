65faa8026969face42997b2d372273fd5f974541
Daniel Santiago Vera Arias <dsveraa.ing@gmail.com>
2026-03-10 10:39:18 -0500

    Feat: En esta modificacion lo que hicimos fue trabajas con condicionales para mostrar etiquetas,  filtro consecutivos, y simplificar la forma en la que se cargan las plantillas sin necesidad de tener que cargar las manualmente si no con el metodo loader.get_template añadiendo su ubicacion en settings.

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
+#from django.template import loader
+from django.template.loader import get_template
 
 class Persona(object):
     def __init__(self, nombre, apellido):
@@ -18,19 +20,30 @@ def saludo(request):
     temas2 = ["Indice 0","Indice 1", "Indice 2","Indice 3", "Indice 4"]
 
     # Cargamos nuestra plantilla 
-    doc_externo = open("C:/Users/danim/Proyectos/Django_pildoras/Proyecto1/Proyecto1/Plantillas/miplantilla1.html")
+    #doc_externo = open("C:/Users/danim/Proyectos/Django_pildoras/Proyecto1/Proyecto1/Plantillas/miplantilla1.html")
     #Creamos el objeto tipo Template
-    platilla = Template(doc_externo.read())
+    #platilla = Template(doc_externo.read())
     
-    doc_externo.close()
+    #doc_externo.close()
+    
+    # Cargar plantilla mas elegante desde eel metodo loader con django
+    #doc_externo = loader.get_template("miplantilla1.html")
+    doc_externo = get_template("miplantilla1.html")
 
     # Crear contexto
-    contexto = Context({"nombre_persona": nombre, "apellido_persona": apellido, "apellido_2": "Arias", "fecha_ahora": fecha_ahora,
+    #contexto = Context({"nombre_persona": nombre, "apellido_persona": apellido, "apellido_2": "Arias", "fecha_ahora": fecha_ahora,
+    #                    "objeto_persona1_nom": p1.nombre, "objeto_persona1_ap": p1.apellido, "temas":["Plantillas","Modelos", "FOrmularios","Listas", "Despliegue"],
+    #                    "temas2": temas2})
+    # EL CONTEXTO AHORA CON EL METODO get_template recibe un diccionario directamente sin necesidad de crear un objeto tipo Context
+    diccionario_contexto = {"nombre_persona": nombre, "apellido_persona": apellido, "apellido_2": "Arias", "fecha_ahora": fecha_ahora,
+                        "objeto_persona1_nom": p1.nombre, "objeto_persona1_ap": p1.apellido, "temas":["Plantillas","Modelos", "FOrmularios","Listas", "Despliegue"],
+                        "temas2": temas2}
+    
+    # Renderizar plantilla (variable)
+    #documento = platilla.render(contexto)
+    documento = doc_externo.render({"nombre_persona": nombre, "apellido_persona": apellido, "apellido_2": "Arias", "fecha_ahora": fecha_ahora,
                         "objeto_persona1_nom": p1.nombre, "objeto_persona1_ap": p1.apellido, "temas":["Plantillas","Modelos", "FOrmularios","Listas", "Despliegue"],
                         "temas2": temas2})
-
-    # Renderizar plantilla (variable)
-    documento = platilla.render(contexto)
 
     return HttpResponse(documento)
 
