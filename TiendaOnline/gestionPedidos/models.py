bc25304f6359241534d267fc48407b489c9883b3
Daniel Santiago Vera Arias <dsveraa.ing@gmail.com>
2026-03-13 11:34:34 -0500

    Feat: Realizamos modificacion a nuestro proyecto,  donde activamos nuestra nueva aplicacion gestion pedidos, tambien creamos nuestro modelos de DB (tablas) y con ello realizamos una migraion lista para guardar en sql3.

--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=32)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=32)
+    seccion = models.CharField(max_length=20)
+    precio = models.IntegerField()
+
+class Pedidos(models.Model):
+    numero_pedido = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
