ddd2597d3953ca752ccd4ee3e3307068a6b4c2e4
Daniel Santiago Vera Arias <dsveraa.ing@gmail.com>
2026-03-16 16:34:28 -0500

    Feat: Realizmos uno metodo dentro de un modelo para que devuelva los parametros de ese modelo a la hora de filtrar por consola shell con el metodo .filter, desde luego anexamos unos pequeños ejemplos de como trabajar con el metodo filter en nuestro .mk.

--- TiendaOnline/insert_datos.mk
+++ TiendaOnline/insert_datos.mk
@@ -13,6 +13,18 @@ art2.save()
 #Metodo create para guardar un nuevo registro
 >> art3 = Articulos.objects.create(nombre='Taladro',seccion='Ferreteria',precio=134)
 
+# INSERTAR VARIOS REGISTROS A LA VEZ
+articulos = [
+	Articulos(nombre="Lampara",seccion= "Decoracion", precio= 70),
+	Articulos(nombre="Pantalon",seccion= "Confeccion", precio= 45),
+	Articulos(nombre="Destornillador",seccion= "Ferreteria", precio= 35),
+	Articulos(nombre="Balon",seccion= "Deportes", precio= 25),
+	Articulos(nombre="Raqueta",seccion= "Deportes", precio= 105),
+	Articulos(nombre="Muñeco",seccion= "Jugueteria", precio= 15),
+	Articulos(nombre="Tren Electrico",seccion= "Jugueteria", precio= 135),
+]
+Articulos.objects.bulk_create(articulos)
+
 # Como actualizar un registro
 >> art.precio = 120
 >> art.save()
@@ -28,3 +40,29 @@ art2.save()
 >> Lista
 
 >> Lista.query.__str__()
+
+# FILTRADO DE OBJETOS METODO (FILTER)
+
+# WHERE
+Articulos.objects.filter(seccion = 'Deportes')
+
+# 1. Modificamos el modelo en el cual agregamos un metodo que devuelve un string de nuestros campos en models.py (def __str__)
+# 2. Cuando modificamos los modelos toca hacer la migrgacion: 
+# 	2.1. python manage.py makemigrations
+#	2.2. python manage.py migrate
+# 3. Ahora si entramos al shell de python nuevamente y ingresamos valores y vemos como nos devuelve un valor en mensaje.
+
+# where con criterio
+Articulos.objects.filter(seccion = 'Deportes', nombre='Mesa')
+
+# criterio de mayor que (>) es igual a (campo__gte=) (__gte= valor)
+Articulos.objects.filter(precio__gte=90)
+# criterio de menor que (<) es igual a (campo__lte=) (__lte= valor)
+Articulos.objects.filter(precio__lte=90)
+
+# Rangos entre tal a tal numero: __range(10,106)
+Articulos.objects.filter(precio__range=(40,100))
+
+# ORDENEAR (ORDER BY)
+Articulos.objects.filter(precio__gte=50).order_by('precio') # ASC
+Articulos.objects.filter(precio__gte=50).order_by('-precio') # DESC
